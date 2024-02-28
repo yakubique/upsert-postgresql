daa46295741b4a34f70499d0305654f54de5f824
Vladimir Levin <vladimirlevinozinki@gmail.com>
2024-02-28 20:17:07 +0900

    v1

---
+++ src/io-helper.ts
@@ -0,0 +1,59 @@
+import * as core from '@actions/core';
+import { isBlank, getBooleanInput, getOptional } from '@yakubique/atils/dist';
+
+enum Inputs {
+    Input = 'input',
+    Host = 'host',
+    Port = 'port',
+    Database = 'db',
+    Username = 'username',
+    Password = 'password',
+    ToFile = 'to_file',
+    FromFile = 'from_file',
+    SSL = 'ssl',
+    ColumnToMatch = 'column_to_match',
+    TableSchema = 'table_schema',
+    TableName = 'table_name'
+}
+
+export interface ActionInputs {
+    input: string;
+    host: string;
+    db: string;
+    port: number;
+    username: string;
+    password: string;
+    toFile: boolean;
+    fromFile: boolean;
+    ssl: boolean;
+    columnToMatch: string;
+    tableSchema: string;
+    tableName: string;
+}
+
+export function getInputs(): ActionInputs {
+    const result: ActionInputs | any = {};
+
+    result.input = `${core.getInput(Inputs.Input, { required: true })}`;
+    result.host = `${core.getInput(Inputs.Host, { required: true })}`;
+    result.db = `${core.getInput(Inputs.Database, { required: true })}`;
+    result.username = `${core.getInput(Inputs.Username, { required: true })}`;
+    result.password = `${core.getInput(Inputs.Password, { required: true })}`;
+    result.columnToMatch = `${core.getInput(Inputs.ColumnToMatch, { required: true })}`;
+    result.tableName = `${core.getInput(Inputs.TableName, { required: true })}`;
+
+    const port = `${core.getInput(Inputs.Port, { required: false })}`;
+    if (isBlank(port)) {
+        result.port = 5432;
+    } else {
+        result.prot = parseInt(port, 10);
+    }
+
+    result.tableSchema = getOptional(Inputs.TableSchema, 'public', { required: false });
+
+    result.toFile = getBooleanInput(Inputs.ToFile, { required: false });
+    result.fromFile = getBooleanInput(Inputs.FromFile, { required: false });
+    result.ssl = getBooleanInput(Inputs.SSL, { required: false });
+
+    return result;
+}
---
+++ src/pgutils.ts
@@ -0,0 +1,136 @@
+import { ClientBase } from 'pg';
+import format from 'pg-format';
+
+export type ColumnInfo = {
+    column_name: string;
+    data_type: string;
+    is_nullable: string;
+    udt_name?: string;
+    column_default?: string;
+    is_generated?: 'ALWAYS' | 'NEVER';
+    identity_generation?: 'ALWAYS' | 'NEVER';
+};
+
+export type QueryValue = string | number | string[];
+export type QueryValues = QueryValue[];
+export type QueryWithValues = { query: string; values?: QueryValues };
+
+export async function columnFeatureSupport(
+    db: ClientBase
+): Promise<{ identity_generation: boolean; is_generated: boolean }> {
+    const result = await db.query(
+        `SELECT EXISTS (
+			SELECT 1 FROM information_schema.columns WHERE table_name = 'columns' AND table_schema = 'information_schema' AND column_name = 'is_generated'
+		) as is_generated,
+		EXISTS (
+			SELECT 1 FROM information_schema.columns WHERE table_name = 'columns' AND table_schema = 'information_schema' AND column_name = 'identity_generation'
+		) as identity_generation;`
+    );
+
+    return result.rows[0];
+}
+
+export async function getTableSchema(
+    db: ClientBase,
+    schema: string,
+    table: string
+): Promise<ColumnInfo[]> {
+    const select = ['column_name', 'data_type', 'is_nullable', 'udt_name', 'column_default'];
+
+    const supported = await columnFeatureSupport(db);
+
+    if (supported.identity_generation) {
+        select.push('identity_generation');
+    }
+
+    if (supported.is_generated) {
+        select.push('is_generated');
+    }
+
+    const selectString = select.join(', ');
+    const result = await db.query(
+        `SELECT ${selectString} FROM information_schema.columns WHERE table_schema = $1 AND table_name = $2`,
+        [schema, table]
+    );
+
+    return result.rows;
+}
+
+export function checkItemAgainstSchema(
+    item: any,
+    columnsInfo: ColumnInfo[],
+    index: number
+) {
+    if (columnsInfo.length === 0) return item;
+    const schema = columnsInfo.reduce((acc, { column_name, data_type, is_nullable }) => {
+        acc[column_name] = { type: data_type.toUpperCase(), nullable: is_nullable === 'YES' };
+        return acc;
+    }, {} as any);
+
+    for (const key of Object.keys(item)) {
+        if (schema[key] === undefined) {
+            throw new Error(`Column '${key}' does not exist in selected table - ${JSON.stringify({
+                itemIndex: index
+            })}`);
+        }
+        if (item[key] === null && !(schema[key] as any)?.nullable) {
+            throw new Error(`Column '${key}' is not nullable -  ${JSON.stringify({
+                itemIndex: index
+            })}`);
+        }
+    }
+
+    return item;
+}
+
+export function upsertOne(
+    item: any,
+    index: number,
+    schema: string,
+    table: string,
+    tableSchema: ColumnInfo[],
+    columnsToMatchOn: string[]
+): QueryWithValues {
+    let values: QueryValues = [];
+
+    item = checkItemAgainstSchema(item, tableSchema, index);
+
+    let valuesLength = values.length + 1;
+    const conflictColumns: string[] = [];
+
+    columnsToMatchOn.forEach((column) => {
+        conflictColumns.push(format('%I', column));
+    });
+    const onConflict = ` ON CONFLICT (${conflictColumns.join(',')}) DO UPDATE `;
+
+    const allKeys = Object.keys(item);
+    const updateColumns = allKeys.filter((column) => !columnsToMatchOn.includes(column));
+    const updates: string[] = [];
+
+    const columnsString = allKeys.map(i => format('%I', i)).join(', ');
+    const allValues = allKeys.map(key => item[key]);
+    const valuesString = allValues.map((_, key) => `$${valuesLength + key}`).join(', ');
+
+    const insertQuery = format(
+        `
+                INSERT INTO %I.%I(${columnsString}) 
+                VALUES(${valuesString})${onConflict}`,
+        schema, table
+    );
+    valuesLength = valuesLength + allValues.length - 1;
+    values.push(...allValues);
+
+
+    for (const column of updateColumns) {
+        updates.push(format(
+            `%I = $${valuesLength + 1}`,
+            column
+        ));
+        values.push(format('%s', item[column] as string));
+        valuesLength = valuesLength + 1;
+    }
+
+    let query = `${insertQuery} SET ${updates.join(', ')}`;
+
+    return { query, values } as QueryWithValues;
+}
